a646d4b3cdc15df73f38cb5528432b4881b3af8a
paulo santos <paulo.santos@gpnet.com.br>
2023-12-27 16:56:09 -0300

    🛒 New: Creating new crud for employees

---
+++ src/crud/CreateEmployees.ts
@@ -0,0 +1,17 @@
+import { PrismaClient, employees } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function createEmployee(employeeData: Omit<employees, 'id'>): Promise<employees> {
+    try {
+        const newEmployee = await prisma.employees.create({
+            data: employeeData,
+        });
+        return newEmployee;
+    } catch (error) {
+        console.error('Erro ao criar funcionário: ', error);
+        throw error;
+    } finally {
+        await prisma.$disconnect();
+    }
+}
