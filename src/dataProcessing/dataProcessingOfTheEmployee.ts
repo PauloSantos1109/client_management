cff9313093365ec3d1c8b16a1c1177289a416138
paulo santos <paulo.santos@gpnet.com.br>
2023-12-29 15:36:27 -0300

    📝  Modify: Alting data in schema prisma

--- src/dataProcessing/dataProcessingOfTheEmployee.ts
+++ src/dataProcessing/dataProcessingOfTheEmployee.ts
@@ -1,9 +1,37 @@
-/* Interface */
-interface InputObject {
-    id: Number;
-    employee: String;
-    cpf: String;
-    dataOfBirth: Date;
-    registrationDate: Date;
-    office: String;
+import { InputObjectEmployee } from "../model/model";
+
+function formatEmployee(employee: string): string {
+    return employee.toLocaleLowerCase();
+}
+
+function formatCPF(cpf: string): string {
+    return cpf.normalize('NFC').replace(/[\u0300-\u036f\W_]/g, '');
+}
+
+/* Date formatting */
+function formatDateString(dateString: Date): Date {
+    try {
+        const date = new Date(dateString);
+        if (isNaN(date.getTime())) {
+            throw new Error("Data inválida.");
+        }
+        return date;
+    } catch (error) {
+        console.error(`Erro ao formatar a data: ${error}`);
+        return new Date();
+    }
+}
+
+export function sendToAPIEmployee(value: InputObjectEmployee): InputObjectEmployee {
+    
+    const outputObject: InputObjectEmployee = {
+        id: value.id,
+        employee: formatEmployee(value.employee),
+        cpf: formatCPF(value.cpf),
+        dataOfBirth: formatDateString(value.dataOfBirth),
+        registrationDate: formatDateString(value.registrationDate),
+        office: value.office,
+    };
+
+    return outputObject;
 }
